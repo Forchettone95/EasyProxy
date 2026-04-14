5fd389fc0c594bc2298b95ecd7196e6ae0c36147
nzo66 <233500038+realbestia1@users.noreply.github.com>
2026-04-15 00:48:50 +0200

    Update maxstream.py

--- extractors/maxstream.py
+++ extractors/maxstream.py
@@ -43,7 +43,7 @@ class MaxstreamExtractor:
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
             "accept-language": "en-US,en;q=0.9",
-            "accept-encoding": "gzip, deflate, br",
+            "accept-encoding": "gzip, deflate",
             "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
             "sec-ch-ua-mobile": "?0",
             "sec-ch-ua-platform": '"Windows"',
